f1a23da4dae0a42f63bfa940934c5307d4bc752b
Shubham-tiwari123 <st540362@gmail.com>
2018-06-25 14:29:56 +0530

    Main file

--- src/javaquestions/JavaQuestions.java
+++ src/javaquestions/JavaQuestions.java
@@ -4,24 +4,28 @@ public class JavaQuestions {
 
     public static void main(String[] args) {
         GeneralQuestions q = new GeneralQuestions();
-        /*q.primeNumber();
+        q.primeNumber();
         q.palindrome();
         q.factorial();
-        q.armstrongNumber();*/
+        q.armstrongNumber();
        
-       SortQuestions s = new SortQuestions();
-       //s.bubbleSort();
-       //s.selectionSort();
+        SortQuestions s = new SortQuestions();
+        s.bubbleSort();
+        s.selectionSort();
        
-       ArrayProblems a = new ArrayProblems();
-       //a.largestElement();
-       //a.secondLargestElement();
-       //a.thirdLargestElement();
-       //a.smallestElement();
-       //a.secondSmallestElement();
-       //a.addTwoMatrix();
-       //a.multiplyMatrix();
-       //a.oddOrEven();
-       a.transposeMatrix();
+        Searching s1 = new Searching();
+        s1.binarySearch();
+        s1.linearSearch();
+        
+        ArrayProblems a = new ArrayProblems();
+        a.largestElement();
+        a.secondLargestElement();
+        a.thirdLargestElement();
+        a.smallestElement();
+        a.secondSmallestElement();
+        a.addTwoMatrix();
+        a.multiplyMatrix();
+        a.oddOrEven();
+        a.transposeMatrix();
     }
 }
