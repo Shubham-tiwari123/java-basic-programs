183517800091210d4402b6ae08251de835e7993e
Shubham-tiwari123 <st540362@gmail.com>
2018-06-29 13:58:33 +0530

    Basic questions

--- src/javaquestions/GeneralQuestions.java
+++ src/javaquestions/GeneralQuestions.java
@@ -76,4 +76,17 @@ public class GeneralQuestions{
         else
             System.out.println("\nNot an armstrong num");
     }
+    
+    public void fibonacciSeries(){
+        int count=10;
+        int n1=0,n2=1,n3;
+        System.out.print("\nEnter a number:-");
+        num = sc.nextInt();
+        for(int i=2;i<count;++i){    
+         n3=n1+n2;    
+         System.out.print(" "+n3);    
+         n1=n2;    
+         n2=n3;    
+        }  
+    }
 }
